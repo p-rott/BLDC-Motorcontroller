35947ce0183912df1bf7ab2727e3e073738c90d9
p-rott <45209365+p-rott@users.noreply.github.com>
2022-04-29 10:41:28 +0200

    Add files via upload

--- i2c_motor_control_test.ino
+++ i2c_motor_control_test.ino
@@ -1,38 +1,52 @@
 #include <Wire.h>
 #include <SimpleFOC.h>
 
+/*
+   Rotary Encoder Wires:
+   Purple - PWM -  Gray
+   Green  - GND -  White
+   Blue   - 5V  -  Black
+
+
+*/
+
+#define SENSORPWM1 PB6
+#define SENSORPWM2 PB7
+
 #define LEDPIN PA7
 #define SLAVEADDRESS 0x8
 #define SDAPIN PB9
 #define SCLPIN PB8
 
-#define SUPPLYVOLTAGE 8
+#define SUPPLYVOLTAGE 8.4
 #define VOLTAGELIMITDRIVER 6
 #define VOLTAGELIMITMOTOR 3
 #define MOTORRESISTANCE 5.6
 
 //#define POLEPAIRS 14
 #define POLEPAIRS 11
-#define CLOSEDLOOP false
+#define CLOSEDLOOP true
 
 #define MESSAGESIZE 8
 #define MAXTARGETVELOCITY 50
+#define MAXTARGETCURRENT 10
+#define MAXTARGETVOLTAGE 6
 
 BLDCMotor motor1 = BLDCMotor(POLEPAIRS);
 BLDCDriver3PWM driver1 = BLDCDriver3PWM(PC0, PC1, PC2, PC13);
-MagneticSensorPWM sensor1 = MagneticSensorPWM(PB6, 4, 904);
+MagneticSensorPWM sensor1 = MagneticSensorPWM(SENSORPWM1, 7, 935);
 
 BLDCMotor motor2 = BLDCMotor(POLEPAIRS);
 BLDCDriver3PWM driver2 = BLDCDriver3PWM(PA0, PA1, PA2, PC14);
-MagneticSensorPWM sensor2 = MagneticSensorPWM(PB7, 4, 904);
+MagneticSensorPWM sensor2 = MagneticSensorPWM(SENSORPWM2, 7, 935);
 
 char message[MESSAGESIZE];
-float motorTargetCurrent = 1;
-float motorTargetVelocity = 1;
+float motorTargetCurrent = 0;
+float motorTargetVelocity = 0;
+float motorTargetVoltage = 0;
 
 void setup()
 {
-  pinMode(LEDPIN, OUTPUT);
   blink(2, 100);
 
   initI2C();
@@ -44,15 +58,13 @@ void setup()
 void loop()
 {
   if (CLOSEDLOOP) {
-    motor1.target = motorTargetCurrent;
-    motor2.target = motorTargetCurrent;
     // main FOC algorithm function
     motor1.loopFOC();
     motor2.loopFOC();
 
     // Motion control function
-    motor1.move();
-    motor2.move();
+    motor1.move(motorTargetVoltage);
+    motor2.move(motorTargetVoltage);
   } else {
     motor1.move(motorTargetVelocity);
     motor2.move(motorTargetVelocity);
@@ -60,6 +72,8 @@ void loop()
 }
 
 void blink(int amount, int del) {
+  pinMode(LEDPIN, OUTPUT);
+
   for (int i = 0; i < amount; i++) {
     digitalWrite(LEDPIN, HIGH);
     delay(del);
@@ -77,6 +91,9 @@ void initI2C() {
 }
 
 void initMotors() {
+  pinMode(SENSORPWM1, INPUT);
+  pinMode(SENSORPWM2, INPUT);
+
   if (CLOSEDLOOP) {
     sensor1.init();
     motor1.linkSensor(&sensor1);
@@ -88,7 +105,7 @@ void initMotors() {
   motor1.linkDriver(&driver1);
 
   motor1.voltage_limit = VOLTAGELIMITMOTOR;
-  motor1.phase_resistance = MOTORRESISTANCE;
+  //motor1.phase_resistance = MOTORRESISTANCE;
   motor1.voltage_sensor_align = VOLTAGELIMITMOTOR;
 
   if (CLOSEDLOOP) {
@@ -112,7 +129,7 @@ void initMotors() {
   motor2.linkDriver(&driver2);
 
   motor2.voltage_limit = VOLTAGELIMITMOTOR;
-  motor2.phase_resistance = MOTORRESISTANCE;
+  //motor2.phase_resistance = MOTORRESISTANCE;
   motor2.voltage_sensor_align = VOLTAGELIMITMOTOR;
 
   if (CLOSEDLOOP) {
@@ -135,15 +152,22 @@ void receiveFun (int bytes)
     i++;
   }
   /*
-  if(message[0] != 0xFF) {
+    if(message[0] != 0xFF) {
     blink(3,100);
     return;
-  }
+    }
   */
   float in = message[1];
   in = in - 128;
-  in = in / 128 * MAXTARGETVELOCITY;
-  motorTargetVelocity = in;
+
+
+  if (CLOSEDLOOP) {
+    in = in / 128 * MAXTARGETVOLTAGE;
+    motorTargetVoltage = in;
+  } else {
+    in = in / 128 * MAXTARGETVELOCITY;
+    motorTargetVelocity = in;
+  }
   /*
     char rec = Wire.read();
     message[i] = rec;
